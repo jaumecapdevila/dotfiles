ee43a253f2927975bf4b4f631e447a226b20fc03
jaumecapdevila <contact@jaumecapdevila.net>
2021-01-29 12:45:10 +0100

    Update p10k config

--- shell/alias.sh
+++ shell/alias.sh
@@ -173,6 +173,7 @@ alias ci="composer install"
 alias cu="composer update"
 alias php8cli="docker run --rm -it php:8.0.0RC4-cli php -a"
 alias php74="/usr/local/Cellar/php@7.4/7.4.14_1/bin/php"
+alias php72="/usr/local/Cellar/php@7.2/7.2.34_1/bin/php"
 
 # Golang
 alias gog='go get'
