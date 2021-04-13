788fdd0766e6e155b127258c6e78f0be67ca898d
jaumecapdevila <contact@jaumecapdevila.net>
2021-04-13 11:28:35 +0200

    Add some external deps

--- shell/dep/antigen.zsh
+++ shell/dep/antigen.zsh
@@ -2,6 +2,8 @@
 source /usr/local/share/antigen/antigen.zsh
 
 # Bundle external plugins
+antigen bundle chrissicool/zsh-256color
+antigen bundle djui/alias-tips
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
