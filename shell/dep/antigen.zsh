28675e711271359c2c0a04a7087cfa8a6a88a809
jaumecapdevila <contact@jaumecapdevila.net>
2021-04-13 11:52:37 +0200

    More cleanup

--- shell/dep/antigen.zsh
+++ shell/dep/antigen.zsh
@@ -9,8 +9,6 @@ antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle unixorn/fzf-zsh-plugin
 antigen theme romkatv/powerlevel10k
-# antigen bundle mafredri/zsh-async
-# antigen bundle sindresorhus/pure
 
 # Load plugins
 antigen apply
