1f9a8daf1fd17b2ffc6f3034b80b9177942aa402
jaumecapdevila <contact@jaumecapdevila.net>
2021-04-15 18:25:23 +0200

    Update some configs

--- shell/config/styling.zsh
+++ shell/config/styling.zsh
@@ -1,3 +1,3 @@
-# [[ ! -f "$DOTFILES_PATH/shell/config/themes/liquid.zsh" ]] || source "$DOTFILES_PATH/shell/config/themes/liquid.zsh"
+[[ ! -f "$DOTFILES_PATH/shell/config/themes/liquid.zsh" ]] || source "$DOTFILES_PATH/shell/config/themes/liquid.zsh"
 # [[ ! -f "$DOTFILES_PATH/shell/config/themes/minimal.zsh" ]] || source "$DOTFILES_PATH/shell/config/themes/minimal.zsh"
-[[ ! -f "$DOTFILES_PATH/shell/config/themes/p10k.zsh" ]] || source "$DOTFILES_PATH/shell/config/themes/p10k.zsh"
+# [[ ! -f "$DOTFILES_PATH/shell/config/themes/p10k.zsh" ]] || source "$DOTFILES_PATH/shell/config/themes/p10k.zsh"
--- shell/dep/antigen.zsh
+++ shell/dep/antigen.zsh
@@ -2,13 +2,12 @@
 
 source /usr/local/share/antigen/antigen.zsh
 
-antigen bundle chrissicool/zsh-256color
 antigen bundle djui/alias-tips
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle unixorn/fzf-zsh-plugin
-antigen theme romkatv/powerlevel10k
-# antigen bundle nojhan/liquidprompt
+# antigen theme romkatv/powerlevel10k
+antigen bundle nojhan/liquidprompt
 # antigen bundle subnixr/minimal
 antigen apply
