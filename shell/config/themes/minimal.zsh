1ede8580850b629dd012cdb6b67129cd7c4053e5
jaumecapdevila <contact@jaumecapdevila.net>
2021-04-15 17:37:58 +0200

    Install and configure minimal theme

---
+++ shell/config/themes/minimal.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+MNML_PROMPT=(mnml_status mnml_cwd mnml_git mnml_keymap)
+MNML_RPROMPT=()
+MNML_INFOLN=()
+MNML_MAGICENTER=()
--- shell/dep/antigen.zsh
+++ shell/dep/antigen.zsh
@@ -9,7 +9,6 @@ antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle unixorn/fzf-zsh-plugin
 # antigen theme romkatv/powerlevel10k
-antigen bundle nojhan/liquidprompt
-# antigen theme eendroroy/alien alien
-
+# antigen bundle nojhan/liquidprompt
+antigen bundle subnixr/minimal
 antigen apply
