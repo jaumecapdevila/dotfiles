250f1ac3f9e4b48a09ee777153eee6a11522af88
jaumecapdevila <contact@jaumecapdevila.net>
2023-08-23 16:11:08 +0200

    :fire: Cleanup old themes

--- config/nvim/lua/plugins/groups/themes.lua
+++ config/nvim/lua/plugins/groups/themes.lua
@@ -23,27 +23,6 @@ return {
   },
 
   {
-    "Mofiqul/dracula.nvim",
-    enabled = false,
-    opts = function() return require("plugins.configs.colors").dracula end,
-    config = function(_, opts)
-      require("dracula").setup(opts)
-      vim.cmd("colorscheme dracula")
-    end,
-  },
-
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    enabled = false,
-    opts = function() return require("plugins.configs.colors").github end,
-    config = function(_, opts)
-      require("github-theme").setup(opts)
-      vim.cmd("colorscheme github_dark_dimmed")
-    end,
-  },
-
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     enabled = false,
@@ -51,17 +30,6 @@ return {
     config = function(_, opts)
       require("rose-pine").setup(opts)
       vim.cmd("colorscheme rose-pine")
-    end,
-  },
-
-  {
-    "LunarVim/synthwave84.nvim",
-    enabled = false,
-    name = "synthwave84",
-    opts = function() return require("plugins.configs.colors").synthwave84 end,
-    config = function(_, opts)
-      require("synthwave84").setup(opts)
-      vim.cmd("colorscheme synthwave84")
     end,
   },
 }
