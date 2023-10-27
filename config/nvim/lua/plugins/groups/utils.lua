053ec2eb3058a516589e90f0135726a4ec90478f
jaumecapdevila <contact@jaumecapdevila.net>
2023-10-27 10:23:42 +0200

    remove undotree

--- config/nvim/lua/plugins/groups/utils.lua
+++ config/nvim/lua/plugins/groups/utils.lua
@@ -4,12 +4,4 @@ return {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
-  {
-    "mbbill/undotree",
-    lazy = false,
-    cmd = "UndotreeToggle",
-    keys = {
-      { "<leader>ut", "<cmd>UndotreeToggle<cr>", desc = "Undotree Toggle" },
-    },
-  },
 }
