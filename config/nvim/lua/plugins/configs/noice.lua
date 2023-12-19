022fc102cab427055a7ccf03bc32e9cb15fa135c
jaumecapdevila <contact@jaumecapdevila.net>
2023-12-19 15:57:39 +0000

    remove comment

--- config/nvim/lua/plugins/configs/noice.lua
+++ config/nvim/lua/plugins/configs/noice.lua
@@ -18,8 +18,6 @@ local options = {
   notify = {
     enabled = false,
   },
-  
-  -- you can enable a preset for easier configuration
   presets = {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
