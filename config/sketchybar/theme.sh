e3efbc29bd0bf5afce4567f711162923120aac6d
jaumecapdevila <contact@jaumecapdevila.net>
2024-01-22 12:25:16 +0100

    start sketchybar config

---
+++ config/sketchybar/theme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Fonts
+export FONT="SF Pro"
+
+# Sizes
+export BAR_HEIGHT=32
+export ITEM_HEIGHT=24
+
+# Margins and paddings
+export GAP=8
+export BAR_PADDING=10
+export ICON_PADDING=4
+export LABEL_PADDING=4
+export ITEM_PADDING=4
+
+# Other
+export RADIUS=5
+
+# Colorscheme
+export BAR_COLOR=0xff292d3e
+export ITEM_BG_COLOR=0xff82aaff
+export ACCENT_COLOR=0xffab47bc
+export ICON_COLOR=0xffe5e9f0
+export LABEL_COLOR=0xffe5e9f0
