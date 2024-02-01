6d2eeb2de9c3c4ba16697b0cfb7a74747d38785f
jaumecapdevila <contact@jaumecapdevila.net>
2024-02-01 12:27:08 +0100

    huge update

--- config/nvim/lua/plugins/groups/themes.lua
+++ config/nvim/lua/plugins/groups/themes.lua
@@ -1,23 +1,13 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    name = "tokyonight",
-    opts = function() return require("plugins.configs.tokyonight") end,
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
-    "marko-cerovac/material.nvim",
-    name = "material",
-    enabled = false,
-    opts = function() return require("plugins.configs.material") end,
-    config = function(_, opts)
-      require("material").setup(opts)
-      -- vim.cmd([[colorscheme material]])
-    end,
-  },
+  "Shatur/neovim-ayu",
+  name = "ayu",
+  opts = function()
+    return {
+      mirage = true
+    }
+  end,
+  config = function(_, opts)
+    require("ayu").setup(opts)
+    vim.cmd([[colorscheme ayu]])
+  end
 }
--- config/shell/zsh/.zshrc
+++ config/shell/zsh/.zshrc
@@ -15,13 +15,13 @@ setopt +o nomatch
 
 # zim plugins
 zstyle ':zim:git' aliases-prefix 'g'
-zstyle ':fzf-tab:*' switch-group ',' '.'
+# zstyle ':fzf-tab:*' switch-group ',' '.'
 
 # Start zim
 source "$ZIM_HOME/init.zsh"
 
 # Load fzf keybindings
-source $HOME/.fzf.zsh
+# source $HOME/.fzf.zsh
 
 # Load Custom Config
 source "$DOTFILES/config/shell/init.sh"
